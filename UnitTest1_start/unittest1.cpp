72a9da2e961d74cd86cfd5086dc2f8a429abe721
Mxenge <213571329@stu.ukzn.ac.za>
2016-02-16 23:04:20 +0200

    last commit for today

--- UnitTest1_start/unittest1.cpp
+++ UnitTest1_start/unittest1.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "..\RoorFinder\RoorFinder.h"
-
+#include <stdexcept>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -42,6 +42,34 @@ namespace UnitTest1_start
 			{
 				double actual = rooter.SquareRoot(v*v);
 				Assert::AreEqual(v, actual, v / 1000);
+			}
+		}
+
+		//Verify that negative inputs throw an exception
+		TEST_METHOD(NegativeRangeTest)
+		{
+			wchar_t message[200];
+			CRoorFinder rooter;
+			for (double v = -0.1; v > -3.0; v = v - 0.5)
+			{
+				try
+				{
+					//Should raise an exception:
+					double  result = rooter.SquareRoot(v);
+
+					_swprintf(message, L"No exception for input %g", v);
+					Assert::Fail(message, LINE_INFO());
+				}
+				catch (std::out_of_range ex)
+				{
+					continue;// Correct exception.
+				}
+				catch (...)
+				{
+					_swprintf(message, L"Incorrect exception for %g", v);
+					Assert::Fail(message, LINE_INFO());
+
+				}
 			}
 		}
 	};
